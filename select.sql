c547f1cd1912e62fd12748bd5bd50f1cb1b99c2e
Juan Cruz Saldaño <70445613+fowardelcac@users.noreply.github.com>
2022-09-05 22:16:28 -0300

    Update select.sql

--- select.sql
+++ select.sql
@@ -18,3 +18,29 @@ FROM employee;
 -- find out all the different genders
 SELECT DISTINCT sex FROM employee;
 
+-- find the number of employees
+SELECT COUNT(emp_id) FROM employee;
+
+-- find the numer of female employees born after 1970
+SELECT COUNT(emp_id) FROM employee
+WHERE sex = 'F' AND birth_date >= '1970-01-01';
+
+-- find the average of employes salaries
+SELECT AVG(salary) FROM employee;
+
+-- find the sum of salary
+SELECT SUM(salary) FROM employee;
+
+--find out how many M abd F ther are (agregation)
+SELECT COUNT(sex), sex FROM employee
+GROUP BY sex;
+
+-- find the total sales from salesman
+SELECT SUM(total_sales) AS sales, emp_id FROM works_with
+GROUP BY emp_id
+ORDER BY sales DESC;
+
+SELECT * FROM works_with;
+-- find how much money spent each client
+SELECT SUM(total_sales), client_id FROM works_with
+GROUP BY client_id;
